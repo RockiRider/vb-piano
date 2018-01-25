d03223efed7d92b8f3d60601d686d62ccaf44625
Tsotne <tsotne24@hotmail.com>
2018-01-25 01:22:54 +0000

    Main Files

---
+++ Piano project/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Piano project")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("CWC")> 
+<Assembly: AssemblyProduct("Piano project")> 
+<Assembly: AssemblyCopyright("Copyright © CWC 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a4305b5e-101b-4cc1-bf47-df2d6b1f725b")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
