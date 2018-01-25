129864e854966541974beebdf012def14ff6f02f
Tsotne <tsotne24@hotmail.com>
2018-01-25 01:25:41 +0000

    Old Version

---
+++ OldVersion/Piano project/Form1.Designer.vb
@@ -0,0 +1,345 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnC = New System.Windows.Forms.Button
+        Me.btnD = New System.Windows.Forms.Button
+        Me.btnE = New System.Windows.Forms.Button
+        Me.btnF = New System.Windows.Forms.Button
+        Me.btnG = New System.Windows.Forms.Button
+        Me.btnA = New System.Windows.Forms.Button
+        Me.btnB = New System.Windows.Forms.Button
+        Me.btnC1 = New System.Windows.Forms.Button
+        Me.btnD1 = New System.Windows.Forms.Button
+        Me.btnE1 = New System.Windows.Forms.Button
+        Me.btnF1 = New System.Windows.Forms.Button
+        Me.btnCq = New System.Windows.Forms.Button
+        Me.btnDq = New System.Windows.Forms.Button
+        Me.btnFq = New System.Windows.Forms.Button
+        Me.btnGq = New System.Windows.Forms.Button
+        Me.btnBb = New System.Windows.Forms.Button
+        Me.btnCq1 = New System.Windows.Forms.Button
+        Me.btnDq1 = New System.Windows.Forms.Button
+        Me.rbConsole = New System.Windows.Forms.RadioButton
+        Me.rbMusic = New System.Windows.Forms.RadioButton
+        Me.rbSteelDrum = New System.Windows.Forms.RadioButton
+        Me.SuspendLayout()
+        '
+        'btnC
+        '
+        Me.btnC.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnC.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnC.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnC.Location = New System.Drawing.Point(12, 65)
+        Me.btnC.Name = "btnC"
+        Me.btnC.Size = New System.Drawing.Size(81, 312)
+        Me.btnC.TabIndex = 1
+        Me.btnC.Text = "C"
+        Me.btnC.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnC.UseVisualStyleBackColor = False
+        '
+        'btnD
+        '
+        Me.btnD.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnD.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnD.Location = New System.Drawing.Point(99, 65)
+        Me.btnD.Name = "btnD"
+        Me.btnD.Size = New System.Drawing.Size(81, 312)
+        Me.btnD.TabIndex = 1
+        Me.btnD.Text = "D"
+        Me.btnD.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnD.UseVisualStyleBackColor = False
+        '
+        'btnE
+        '
+        Me.btnE.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnE.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnE.Location = New System.Drawing.Point(186, 65)
+        Me.btnE.Name = "btnE"
+        Me.btnE.Size = New System.Drawing.Size(81, 312)
+        Me.btnE.TabIndex = 1
+        Me.btnE.Text = "E"
+        Me.btnE.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnE.UseVisualStyleBackColor = False
+        '
+        'btnF
+        '
+        Me.btnF.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnF.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnF.Location = New System.Drawing.Point(273, 65)
+        Me.btnF.Name = "btnF"
+        Me.btnF.Size = New System.Drawing.Size(81, 312)
+        Me.btnF.TabIndex = 1
+        Me.btnF.Text = "F"
+        Me.btnF.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnF.UseVisualStyleBackColor = False
+        '
+        'btnG
+        '
+        Me.btnG.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnG.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnG.Location = New System.Drawing.Point(360, 65)
+        Me.btnG.Name = "btnG"
+        Me.btnG.Size = New System.Drawing.Size(81, 312)
+        Me.btnG.TabIndex = 1
+        Me.btnG.Text = "G"
+        Me.btnG.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnG.UseVisualStyleBackColor = False
+        '
+        'btnA
+        '
+        Me.btnA.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnA.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnA.Location = New System.Drawing.Point(447, 65)
+        Me.btnA.Name = "btnA"
+        Me.btnA.Size = New System.Drawing.Size(81, 312)
+        Me.btnA.TabIndex = 1
+        Me.btnA.Text = "A"
+        Me.btnA.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnA.UseVisualStyleBackColor = False
+        '
+        'btnB
+        '
+        Me.btnB.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnB.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnB.Location = New System.Drawing.Point(534, 65)
+        Me.btnB.Name = "btnB"
+        Me.btnB.Size = New System.Drawing.Size(81, 312)
+        Me.btnB.TabIndex = 1
+        Me.btnB.Text = "B"
+        Me.btnB.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnB.UseVisualStyleBackColor = False
+        '
+        'btnC1
+        '
+        Me.btnC1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnC1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnC1.Location = New System.Drawing.Point(621, 65)
+        Me.btnC1.Name = "btnC1"
+        Me.btnC1.Size = New System.Drawing.Size(81, 312)
+        Me.btnC1.TabIndex = 1
+        Me.btnC1.Text = "C"
+        Me.btnC1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnC1.UseVisualStyleBackColor = False
+        '
+        'btnD1
+        '
+        Me.btnD1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnD1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnD1.Location = New System.Drawing.Point(708, 65)
+        Me.btnD1.Name = "btnD1"
+        Me.btnD1.Size = New System.Drawing.Size(81, 312)
+        Me.btnD1.TabIndex = 1
+        Me.btnD1.Text = "D"
+        Me.btnD1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnD1.UseVisualStyleBackColor = False
+        '
+        'btnE1
+        '
+        Me.btnE1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnE1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnE1.Location = New System.Drawing.Point(795, 65)
+        Me.btnE1.Name = "btnE1"
+        Me.btnE1.Size = New System.Drawing.Size(81, 312)
+        Me.btnE1.TabIndex = 1
+        Me.btnE1.Text = "E"
+        Me.btnE1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnE1.UseVisualStyleBackColor = False
+        '
+        'btnF1
+        '
+        Me.btnF1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnF1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnF1.Location = New System.Drawing.Point(882, 65)
+        Me.btnF1.Name = "btnF1"
+        Me.btnF1.Size = New System.Drawing.Size(81, 312)
+        Me.btnF1.TabIndex = 1
+        Me.btnF1.Text = "F"
+        Me.btnF1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btnF1.UseVisualStyleBackColor = False
+        '
+        'btnCq
+        '
+        Me.btnCq.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnCq.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnCq.Location = New System.Drawing.Point(62, 65)
+        Me.btnCq.Name = "btnCq"
+        Me.btnCq.Size = New System.Drawing.Size(68, 181)
+        Me.btnCq.TabIndex = 2
+        Me.btnCq.Text = "C#"
+        Me.btnCq.UseVisualStyleBackColor = False
+        '
+        'btnDq
+        '
+        Me.btnDq.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnDq.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnDq.Location = New System.Drawing.Point(154, 65)
+        Me.btnDq.Name = "btnDq"
+        Me.btnDq.Size = New System.Drawing.Size(68, 181)
+        Me.btnDq.TabIndex = 2
+        Me.btnDq.Text = "D#"
+        Me.btnDq.UseVisualStyleBackColor = False
+        '
+        'btnFq
+        '
+        Me.btnFq.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnFq.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnFq.Location = New System.Drawing.Point(321, 65)
+        Me.btnFq.Name = "btnFq"
+        Me.btnFq.Size = New System.Drawing.Size(68, 181)
+        Me.btnFq.TabIndex = 2
+        Me.btnFq.Text = "F#"
+        Me.btnFq.UseVisualStyleBackColor = False
+        '
+        'btnGq
+        '
+        Me.btnGq.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnGq.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnGq.Location = New System.Drawing.Point(410, 65)
+        Me.btnGq.Name = "btnGq"
+        Me.btnGq.Size = New System.Drawing.Size(68, 181)
+        Me.btnGq.TabIndex = 2
+        Me.btnGq.Text = "G#"
+        Me.btnGq.UseVisualStyleBackColor = False
+        '
+        'btnBb
+        '
+        Me.btnBb.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnBb.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnBb.Location = New System.Drawing.Point(498, 65)
+        Me.btnBb.Name = "btnBb"
+        Me.btnBb.Size = New System.Drawing.Size(68, 181)
+        Me.btnBb.TabIndex = 2
+        Me.btnBb.Text = "Bb"
+        Me.btnBb.UseVisualStyleBackColor = False
+        '
+        'btnCq1
+        '
+        Me.btnCq1.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnCq1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnCq1.Location = New System.Drawing.Point(669, 65)
+        Me.btnCq1.Name = "btnCq1"
+        Me.btnCq1.Size = New System.Drawing.Size(68, 181)
+        Me.btnCq1.TabIndex = 2
+        Me.btnCq1.Text = "C#1"
+        Me.btnCq1.UseVisualStyleBackColor = False
+        '
+        'btnDq1
+        '
+        Me.btnDq1.BackColor = System.Drawing.SystemColors.ControlText
+        Me.btnDq1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnDq1.Location = New System.Drawing.Point(759, 65)
+        Me.btnDq1.Name = "btnDq1"
+        Me.btnDq1.Size = New System.Drawing.Size(68, 181)
+        Me.btnDq1.TabIndex = 2
+        Me.btnDq1.Text = "D#1"
+        Me.btnDq1.UseVisualStyleBackColor = False
+        '
+        'rbConsole
+        '
+        Me.rbConsole.AutoSize = True
+        Me.rbConsole.Location = New System.Drawing.Point(28, 13)
+        Me.rbConsole.Name = "rbConsole"
+        Me.rbConsole.Size = New System.Drawing.Size(94, 17)
+        Me.rbConsole.TabIndex = 3
+        Me.rbConsole.TabStop = True
+        Me.rbConsole.Text = "Console Notes"
+        Me.rbConsole.UseVisualStyleBackColor = True
+        '
+        'rbMusic
+        '
+        Me.rbMusic.AutoSize = True
+        Me.rbMusic.Location = New System.Drawing.Point(388, 13)
+        Me.rbMusic.Name = "rbMusic"
+        Me.rbMusic.Size = New System.Drawing.Size(84, 17)
+        Me.rbMusic.TabIndex = 3
+        Me.rbMusic.TabStop = True
+        Me.rbMusic.Text = "Music Notes"
+        Me.rbMusic.UseVisualStyleBackColor = True
+        '
+        'rbSteelDrum
+        '
+        Me.rbSteelDrum.AutoSize = True
+        Me.rbSteelDrum.Location = New System.Drawing.Point(737, 13)
+        Me.rbSteelDrum.Name = "rbSteelDrum"
+        Me.rbSteelDrum.Size = New System.Drawing.Size(108, 17)
+        Me.rbSteelDrum.TabIndex = 3
+        Me.rbSteelDrum.TabStop = True
+        Me.rbSteelDrum.Text = "Steel Drum Notes"
+        Me.rbSteelDrum.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(972, 389)
+        Me.Controls.Add(Me.rbSteelDrum)
+        Me.Controls.Add(Me.rbMusic)
+        Me.Controls.Add(Me.rbConsole)
+        Me.Controls.Add(Me.btnDq1)
+        Me.Controls.Add(Me.btnCq1)
+        Me.Controls.Add(Me.btnBb)
+        Me.Controls.Add(Me.btnGq)
+        Me.Controls.Add(Me.btnFq)
+        Me.Controls.Add(Me.btnDq)
+        Me.Controls.Add(Me.btnCq)
+        Me.Controls.Add(Me.btnF1)
+        Me.Controls.Add(Me.btnE1)
+        Me.Controls.Add(Me.btnD1)
+        Me.Controls.Add(Me.btnC1)
+        Me.Controls.Add(Me.btnB)
+        Me.Controls.Add(Me.btnA)
+        Me.Controls.Add(Me.btnG)
+        Me.Controls.Add(Me.btnF)
+        Me.Controls.Add(Me.btnE)
+        Me.Controls.Add(Me.btnD)
+        Me.Controls.Add(Me.btnC)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnC As System.Windows.Forms.Button
+    Friend WithEvents btnD As System.Windows.Forms.Button
+    Friend WithEvents btnE As System.Windows.Forms.Button
+    Friend WithEvents btnF As System.Windows.Forms.Button
+    Friend WithEvents btnG As System.Windows.Forms.Button
+    Friend WithEvents btnA As System.Windows.Forms.Button
+    Friend WithEvents btnB As System.Windows.Forms.Button
+    Friend WithEvents btnC1 As System.Windows.Forms.Button
+    Friend WithEvents btnD1 As System.Windows.Forms.Button
+    Friend WithEvents btnE1 As System.Windows.Forms.Button
+    Friend WithEvents btnF1 As System.Windows.Forms.Button
+    Friend WithEvents btnCq As System.Windows.Forms.Button
+    Friend WithEvents btnDq As System.Windows.Forms.Button
+    Friend WithEvents btnFq As System.Windows.Forms.Button
+    Friend WithEvents btnGq As System.Windows.Forms.Button
+    Friend WithEvents btnBb As System.Windows.Forms.Button
+    Friend WithEvents btnCq1 As System.Windows.Forms.Button
+    Friend WithEvents btnDq1 As System.Windows.Forms.Button
+    Friend WithEvents rbConsole As System.Windows.Forms.RadioButton
+    Friend WithEvents rbMusic As System.Windows.Forms.RadioButton
+    Friend WithEvents rbSteelDrum As System.Windows.Forms.RadioButton
+
+End Class
---
+++ OldVersion/Piano project/Form1.vb
@@ -0,0 +1,230 @@
+﻿Public Class Form1
+
+    Private Sub RadioButton1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnC.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(200, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.C, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.C_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rbConsole.CheckedChanged
+
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCq.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(250, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.C_s, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.Cq_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnD_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnD.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(300, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.D, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.D_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnE_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnE.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(400, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.E, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.E_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnF_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnF.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(450, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.F, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.F_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnG_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnG.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(550, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.G, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.G_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnA_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnA.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(650, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.A, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.A_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnB_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnB.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(750, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.B, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.B_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnC1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnC1.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(800, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.C1, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.C1_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnD1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnD1.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(900, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.D1, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.D1_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnE1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnE1.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(1000, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.E1, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.E1_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnF1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnF1.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(1050, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.F1, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.F1_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnDq_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDq.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(350, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.D_s, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.Dq_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnFq_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnFq.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(500, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.F_s, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.Fq_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnGq_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGq.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(600, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.G_s, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.Gq_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnBb_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBb.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(700, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.Bb, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.Bb_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnCq1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCq1.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(850, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.C_s1, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.Cq1_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+
+    Private Sub btnDq1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDq1.Click
+        If rbConsole.Checked = True Then
+            Console.Beep(950, 500)
+        End If
+        If rbMusic.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.D_s1, AudioPlayMode.Background)
+        End If
+        If rbSteelDrum.Checked = True Then
+            My.Computer.Audio.Play(My.Resources.Dq1_Drum, AudioPlayMode.Background)
+        End If
+    End Sub
+End Class
